49d352d2b26d3d28e7d83c5eda7fb65397ea9ec2
Nick <nick22783@gmail.com>
2023-11-24 14:05:01 +0300

    add mock users

--- src/composables/employeeListStore.ts
+++ src/composables/employeeListStore.ts
@@ -1,6 +1,7 @@
 import { ref } from 'vue'
 import type { Ref } from 'vue'
 import type { RouteParamValue } from 'vue-router'
+import { mockUsers } from '@/mocks/mockUsers'
 
 export interface Employee {
   id: string
@@ -11,12 +12,12 @@ export interface Employee {
   address: string
 }
 
-export const employeeList: Ref<Employee[]> = ref([])
+export const employeeList: Ref<Employee[]> = ref(mockUsers)
 
 export const addEmployee = (employee: Employee) => employeeList.value.push(employee)
 export const editEmployee = (editedEmployee: Employee) => {
   const employeeInTheListIndex = employeeList.value.findIndex(
-    (item) => item.id === editedEmployee.id
+    (item) => item.id === editedEmployee.id,
   )
   employeeList.value[employeeInTheListIndex] = editedEmployee
 }
