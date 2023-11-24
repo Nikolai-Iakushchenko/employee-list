f0e6a82198534ed9310c51eca097783764c42144
Nick <nick22783@gmail.com>
2023-11-24 12:19:06 +0300

    add i18n messages

---
+++ src/constants/i18n.ts
@@ -0,0 +1,28 @@
+const messages = {
+  en: {
+    message: {
+      addEmployee: 'add employee',
+      address: 'address',
+      age: 'age',
+      cancel: 'cancel',
+      employeeList: 'employee ist',
+      experience: 'experience',
+      lastName: 'last name',
+      name: 'name',
+      save: 'save',
+    },
+  },
+  ru: {
+    message: {
+      addEmployee: 'добавить сотрудника',
+      address: 'адрес',
+      age: 'возраст',
+      cancel: 'отмена',
+      employeeList: 'cписок сотрудников',
+      experience: 'стаж',
+      lastName: 'фамилия',
+      name: 'имя',
+      save: 'сохранить',
+    },
+  },
+}
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({})
 
 const app = createApp(App)
 
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
